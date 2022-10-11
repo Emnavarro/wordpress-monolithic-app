22cb248f761241f390e4025cbfec30630d81069d
Emanuel Navarro <navarrof.emanuel96@gmail.com>
2022-10-11 10:19:15 -0400

    Adding ec2 ssm role

--- ec2.tf
+++ ec2.tf
@@ -16,12 +16,16 @@ resource "aws_instance" "wordpress" {
   subnet_id = module.vpc.public_subnets[count.index]
   security_groups = ["${aws_security_group.sg.id}"]
   user_data = "${file("userdata.sh")}"
+  iam_instance_profile = aws_iam_instance_profile.ec2-ssm-instanceprofile.name
 
 
   #Takes the subnet string array from the module
   count = length(module.vpc.public_subnets)
 }
 
+#######################################
+#AWS Security wordpress security group#
+#######################################
 
 resource "aws_security_group" "sg" {
   vpc_id = module.vpc.vpc_id
@@ -48,3 +52,40 @@ variable "sg-ports" {
   type = list(number)
   default = [22,80,443]
 }
+
+#IAM Role for Wordpress // Parameter store
+resource "aws_iam_role" "ec2-ssm-role" {
+  name = "ec2-ssm-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name = "Iam role for ssm parameter store"
+  }
+}
+#Attach the policy with the IAM role
+resource "aws_iam_role_policy_attachment" "ec2-policy-attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role = aws_iam_role.ec2-ssm-role.name
+}
+
+#Allows IAM role attachment with an ec2 instance
+resource "aws_iam_instance_profile" "ec2-ssm-instanceprofile" {
+  name = "ec2-ssm-profile"
+  role = aws_iam_role.ec2-ssm-role.name
+}
+
--- userdata.sh
+++ userdata.sh
@@ -1,10 +1,21 @@
 #!/bin/bash -xe
 
 # Setpassword & DB Variables
-DBName='REPLACEME'
-DBUser='REPLACEME'
-DBPassword='REPLACEME'
-DBRootPassword='REPLACEME'
+DBPassword=$(aws ssm get-parameters --region us-east-1 --names /Wordpress/DBPassword --with-decryption --query Parameters[0].Value)
+DBPassword=`echo $DBPassword | sed -e 's/^"//' -e 's/"$//'`
+
+DBRootPassword=$(aws ssm get-parameters --region us-east-1 --names /Wordpress/DBRootPassword --with-decryption --query Parameters[0].Value)
+DBRootPassword=`echo $DBRootPassword | sed -e 's/^"//' -e 's/"$//'`
+
+DBUser=$(aws ssm get-parameters --region us-east-1 --names /Wordpress/DBUser --query Parameters[0].Value)
+DBUser=`echo $DBUser | sed -e 's/^"//' -e 's/"$//'`
+
+DBName=$(aws ssm get-parameters --region us-east-1 --names /Wordpress/DBName --query Parameters[0].Value)
+DBName=`echo $DBName | sed -e 's/^"//' -e 's/"$//'`
+
+DBEndpoint=$(aws ssm get-parameters --region us-east-1 --names /Wordpress/DBEndpoint --query Parameters[0].Value)
+DBEndpoint=`echo $DBEndpoint | sed -e 's/^"//' -e 's/"$//'`
+
 
 # System Updates
 yum -y update
